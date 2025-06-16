e2efc0d8c98750eb4186d776dcc0455c9372404b
Harika Ejju <haarikamohanrao@gmail.com>
2025-06-16 15:28:51 +0530

    Update hahha.py

--- hahha.py
+++ hahha.py
@@ -1,10 +1,5 @@
-def longestIncreasingSubsequence(nums, k):
-  n = len(nums)
-dp = [1] * n
-
-for i in range(1, n):
-    for j in range(i):
-  if nums[i] > nums[j] and nums[i] - nums[j] <= k:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-      return max(dp)
+a= 2
+b=3
+c=a+b
+d=a-b
+print(c,d)
