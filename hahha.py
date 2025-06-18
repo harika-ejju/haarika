788b65df6ce210ca565f3861009bdb3f7366a238
Harika Ejju <haarikamohanrao@gmail.com>
2025-06-18 12:10:40 +0530

    Update hahha.py

--- hahha.py
+++ hahha.py
@@ -1,5 +1,21 @@
-a= 2
-b=3
-c=a+b
-d=a-b
-print(c,d)
+def binary_search(arr, target)
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+        return mid
+        elif arr[mid] < target
+            left = mid + 1
+        else
+            right = mid - 1
+
+    return -1
+
+# Example usage:
+arr = [1, 3, 5, 7, 9, 11]
+target = 7
+result = binary_search(arr target)
+print("Target found at index:", result)
